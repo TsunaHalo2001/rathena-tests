a99b13a63b2a13b89a8bfdc7a8c402f7f7ff7500
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2026-02-28 17:23:39 -0500

    Updated a defined Constant

--- src/map/skills/npc/breakhelm.cpp
+++ src/map/skills/npc/breakhelm.cpp
@@ -7,5 +7,5 @@ SkillBreakHelm::SkillBreakHelm() : WeaponSkillImpl(NPC_HELMBRAKE) {
 }
 
 void SkillBreakHelm::applyAdditionalEffects(block_list *src, block_list *target, uint16 skill_lv, t_tick tick, int32 attack_type, enum damage_lv dmg_lv) const {
-	skill_break_equip(src,target, EQP_HELM, 150*skill_lv, BCT_ENEMY);
+	skill_break_equip(src,target, EQP_HEAD_TOP, 150*skill_lv, BCT_ENEMY);
 }
