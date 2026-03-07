Hrishi29x/rathena

Split up leftover skills 2 (#9883)
Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

src/map/skills/acolyte/skill_factory_acolyte.cpp
@@ -86,6 +86,7 @@
 #include "redemptio.cpp"
 #include "renovatio.cpp"
 #include "reparatio.cpp"
+#include "resurrection.cpp"
 #include "rideinlightening.cpp"
 #include "ruwach.cpp"
 #include "sanctuary.cpp"
@@ -158,6 +159,8 @@ std::unique_ptr<const SkillImpl> SkillFactoryAcolyte::create(const e_skill skill
 			return std::make_unique<SkillSilentium>();
 		case AB_VITUPERATUM:
 			return std::make_unique<SkillVituperatum>();
+		case ALL_RESURRECTION:
+			return std::make_unique<SkillResurrection>();
 		case AL_ANGELUS:
 			return std::make_unique<SkillAngelus>();
 		case AL_BLESSING:

src/map/skills/homunculus/skill_factory_homunculus.cpp
@@ -30,12 +30,14 @@
 #include "homunculus_lightofregene.cpp"
 #include "homunculus_magmaflow.cpp"
 #include "homunculus_midnightfrenzy.cpp"
+#include "homunculus_moonlight.cpp"
 #include "homunculus_needleofparalyze.cpp"
 #include "homunculus_needlestinger.cpp"
 #include "homunculus_overedboost.cpp"
 #include "homunculus_painkiller.cpp"
 #include "homunculus_poisonmist.cpp"
 #include "homunculus_pyroclastic.cpp"
+#include "homunculus_sbr44.cpp"
 #include "homunculus_silentbreeze.cpp"
 #include "homunculus_silverveinrush.cpp"
 #include "homunculus_sonicclaw.cpp"
@@ -62,9 +64,9 @@ std::unique_ptr<const SkillImpl> SkillFactoryHomunculus::create(const e_skill sk
 		case HFLI_FLEET:
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case HFLI_MOON:
-			return std::make_unique<WeaponSkillImpl>(skill_id);
+			return std::make_unique<SkillMoonlight>();
 		case HFLI_SBR44:
-			return std::make_unique<WeaponSkillImpl>(skill_id);
+			return std::make_unique<SkillSBR44>();
 		case HFLI_SPEED:
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case HLIF_AVOID:

TsunaHalo2001/rathena-tests

src/map/skills/acolyte/skill_factory_acolyte.cpp
@@ -86,6 +86,7 @@
 #include "redemptio.hpp"
 #include "renovatio.hpp"
 #include "reparatio.hpp"
+#include "resurrection.hpp"
 #include "rideinlightening.hpp"
 #include "ruwach.hpp"
 #include "sanctuary.hpp"
@@ -158,6 +159,8 @@ std::unique_ptr<const SkillImpl> SkillFactoryAcolyte::create(const e_skill skill
 			return std::make_unique<SkillSilentium>();
 		case AB_VITUPERATUM:
 			return std::make_unique<SkillVituperatum>();
+		case ALL_RESURRECTION:
+			return std::make_unique<SkillResurrection>();
 		case AL_ANGELUS:
 			return std::make_unique<SkillAngelus>();
 		case AL_BLESSING:

src/map/skills/homunculus/skill_factory_homunculus.cpp
@@ -30,12 +30,14 @@
 #include "homunculus_lightofregene.hpp"
 #include "homunculus_magmaflow.hpp"
 #include "homunculus_midnightfrenzy.hpp"
+#include "homunculus_moonlight.hpp"
 #include "homunculus_needleofparalyze.hpp"
 #include "homunculus_needlestinger.hpp"
 #include "homunculus_overedboost.hpp"
 #include "homunculus_painkiller.hpp"
 #include "homunculus_poisonmist.hpp"
 #include "homunculus_pyroclastic.hpp"
+#include "homunculus_sbr44.hpp"
 #include "homunculus_silentbreeze.hpp"
 #include "homunculus_silverveinrush.hpp"
 #include "homunculus_sonicclaw.hpp"
@@ -62,9 +64,9 @@ std::unique_ptr<const SkillImpl> SkillFactoryHomunculus::create(const e_skill sk
 		case HFLI_FLEET:
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case HFLI_MOON:
-			return std::make_unique<WeaponSkillImpl>(skill_id);
+			return std::make_unique<SkillMoonlight>();
 		case HFLI_SBR44:
-			return std::make_unique<WeaponSkillImpl>(skill_id);
+			return std::make_unique<SkillSBR44>();
 		case HFLI_SPEED:
 			return std::make_unique<StatusSkillImpl>(skill_id);
 		case HLIF_AVOID: